57544f5241a1c61669723d0c39cf9c42d27d27a4
Kim MinChan <hialscks@gmail.com>
2026-09-16 23:48:35 +0900

    feat: add Purchase model for handling purchase data

---
+++ src/main/java/dev/training/back/purchase/model/Purchase.java
@@ -0,0 +1,17 @@
+package dev.training.back.purchase.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+public class Purchase {
+    
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+    private Integer UnitPrice;
+    private LocalDateTime purchasedAt;
+}
