4a451a2086cb80eabe2e9f18a3edd1038cd531ef
Kim MinChan <hialscks@gmail.com>
2026-09-22 20:26:11 +0900

    builder 추가

--- src/main/java/dev/training/back/purchase/model/Purchase.java
+++ src/main/java/dev/training/back/purchase/model/Purchase.java
@@ -2,11 +2,17 @@ package dev.training.back.purchase.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter 
+@Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Purchase {
     
     private Long id;
